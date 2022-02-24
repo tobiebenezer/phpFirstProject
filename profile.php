b2fab591cdd4ff9d1492a46c020e6c5a4a3e1995
tobiebenezer <tobisamuel70@gmail.com>
2022-02-24 04:40:59 +0100

    First commit of static profile page withour images

---
+++ profile.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./profile.css">
+    <title>profile</title>
+</head>
+<body>
+    <header>
+    <nav>
+    <div class="logo">
+    <img src="" alt="">
+    <H1>PalmBock</H1></div>
+    <ul class="navi">
+     <li><a href="#">HOME</a></li>
+     <li><a href="#">PROFILE</a></li>
+     <li><a href="#">USERS</a></li>
+     <li><a href="#">LOGIN</a></li>
+     <li><a href="#">REGISTER</a></li>
+    </ul>
+    </nav>
+    </header>
+    <main>
+    <section>
+    <div class="top">
+    <span><img src="" alt="profile icon"> <h2>My Profile</h2></span>
+    </div>
+    <div class="body">
+        <div class='profile-table'>
+            <div class="profile-pic">
+                <div>
+
+                    <img src="" alt="Profile Avatar">
+                    <p> <span>Joe Doe</span> </br>
+                    You are welcome</p>
+                </div>
+            </div>
+            <div class="profile-items">
+                <table>
+                <tr>
+                    <td>Name</td> <td>Joe Doe</td>
+                </tr>
+                <tr>
+                    <td>Mobile Number</td> <td>0000 0000 000</td>
+                </tr>
+                <tr>
+                    <td>Email</td> <td>joedoe@mail.com</td>
+                </tr>
+                <tr>
+                    <td>Address</td> <td>Hall Mark Street</td>
+                </tr>
+                <tr>
+                    <td>Gender</td> <td>Non-Binary</td>
+                </tr>
+                <tr>
+                    <td>Marital Status</td> <td>Single</td>
+                </tr>
+                <tr>
+                    <td>State of Origin</td> <td>Yoke</td>
+                </tr>
+                <tr>
+                    <td>Gender</td> <td>Non-Binary</td>
+                </tr>
+                <tr>
+                    <td>hobbies</td> <td>Danding, reading, writing  singing</td>
+                </tr>
+                
+                
+                </table>
+                <button type="submit" id= 'edit'>Edit Profile</button>
+            </div>
+
+        </div>
+    </div>
+    </section>
+    </main>
+        
+</body>
+</html>
